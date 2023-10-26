8b28989aac47b2607913095c76c2002aa3f0ee91
Akshay Lokur <alokur@apple.com>
2023-10-26 17:58:51 +0800

    Create and move paddles.

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong!")
+screen.tracer(0)  # to disable automatic screen tracing / refresh / animation
+
+left_paddle = Paddle(x_pos=350, y_pos=0)
+right_paddle = Paddle(x_pos=-350, y_pos=0)
+
+screen.listen()
+screen.onkey(left_paddle.go_up, "Up")
+screen.onkey(left_paddle.go_down, "Down")
+screen.onkey(right_paddle.go_up, "w")
+screen.onkey(right_paddle.go_down, "s")
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+
+screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.pu()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(x_pos, y_pos)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
