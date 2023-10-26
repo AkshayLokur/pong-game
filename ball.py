a4c0b8709719a91f3bf3010d807a5b65487329ab
Akshay Lokur <alokur@apple.com>
2023-10-26 21:11:14 +0800

    detect collisions

--- ball.py
+++ ball.py
@@ -8,7 +8,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.pu()
+        self.x_move = 10
+        self.y_move = 10
 
     def move_ball(self):
         time.sleep(0.1)
-        self.goto(self.xcor() + 10, self.ycor() + 10)
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        print("bounce_x")
--- main.py
+++ main.py
@@ -8,14 +8,14 @@ screen.bgcolor("black")
 screen.title("Pong!")
 screen.tracer(0)  # to disable automatic screen tracing / refresh / animation
 
-left_paddle = Paddle(x_pos=350, y_pos=0)
-right_paddle = Paddle(x_pos=-350, y_pos=0)
+left_paddle = Paddle(x_pos=-350, y_pos=0)
+right_paddle = Paddle(x_pos=350, y_pos=0)
 
 screen.listen()
-screen.onkey(left_paddle.go_up, "Up")
-screen.onkey(left_paddle.go_down, "Down")
-screen.onkey(right_paddle.go_up, "w")
-screen.onkey(right_paddle.go_down, "s")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 ball = Ball()
 
@@ -24,5 +24,14 @@ is_game_on = True
 while is_game_on:
     ball.move_ball()
     screen.update()
+
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with the paddles
+    if ball.xcor() > 320 and ball.distance(right_paddle) < 20 or \
+            ball.xcor() < 320 and ball.distance(left_paddle) < 20:
+        ball.bounce_x()
 
 screen.exitonclick()
